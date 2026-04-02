48a3357f030804b7f2d972396b9c38d4f4500ef6
aldre-arch <keyzar2002@gmail.com>
2026-04-02 08:51:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -753,8 +753,9 @@ def show_detail(row, full_df):
         st.subheader("Sensing System & Feature")
         st.write(f"**Sensing System :** {sensing_list}")
         st.write(f"**Feature :** {feature_list}")
+
         # Opsi: Jika ingin menambahkan tombol kecil khusus di dalam kolom
-            if st.button("▶️ Play Video Demo", key=f"yt_inline_{row.name}"):
+            if st.button("▶️ Play Video Demo", key=f"yt_inline_{video_url}"):
                 handle_share_logging(st.session_state.username, brand, model, "YouTube")
                 js_yt = f'window.open("{video_url}", "_blank").focus();'
                 st.components.v1.html(f'<script>{js_yt}</script>', height=0)
